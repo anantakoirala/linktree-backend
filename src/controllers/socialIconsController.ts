133156642524e192953a1de53ed49d838ba83b81
anantakoirala <ananta.drn2049@gmail.com>
2025-01-23 15:12:51 +0545

    edit social icons

--- src/controllers/socialIconsController.ts
+++ src/controllers/socialIconsController.ts
@@ -81,3 +81,69 @@ export const updateSocialIconStatus = async (
     next(error);
   }
 };
+export const getSingleSocialIcon = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { socialIconId } = req.params;
+    console.log("id", socialIconId);
+    if (!req.userId) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    const socialIcon = await SocialIcon.findOne({
+      _id: socialIconId,
+      owner: req.userId,
+    });
+
+    if (!socialIcon) {
+      return res.status(404).json({ success: false, message: "Nof found" });
+    }
+
+    return res.status(200).json({ success: true, socialIcon });
+  } catch (error) {}
+};
+
+export const updateSocialIcon = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { socialIconId } = req.params;
+    const { data } = req.body;
+    if (!req.userId) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+    const isSocialIconAvailable = await SocialIcon.findOne({
+      owner: req.userId,
+      _id: socialIconId,
+    });
+
+    if (!isSocialIconAvailable) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Social icon not found" });
+    }
+
+    const result = await SocialIcon.findByIdAndUpdate(
+      isSocialIconAvailable._id, // Find by ID
+      { $set: { value: data.value } },
+      { new: true }
+    );
+    if (!result) {
+      return res.status(404).json({
+        success: false,
+        message: "Failed to update social icon status",
+      });
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Social icon updated successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/socialIconRoute.ts
+++ src/routes/socialIconRoute.ts
@@ -2,7 +2,9 @@ import express from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
 import {
   getAllIcons,
+  getSingleSocialIcon,
   saveSocialIcons,
+  updateSocialIcon,
   updateSocialIconStatus,
 } from "../controllers/socialIconsController";
 
@@ -10,10 +12,20 @@ const router = express.Router();
 
 router.post("/save-social-icons", authMiddleware, saveSocialIcons);
 router.get("/get-all-icons", authMiddleware, getAllIcons);
+router.get(
+  "/single-social-icon/:socialIconId",
+  authMiddleware,
+  getSingleSocialIcon
+);
 router.patch(
   "/update-social-icon-status/:socialIconId",
   authMiddleware,
   updateSocialIconStatus
+);
+router.patch(
+  "/update-social-icon/:socialIconId",
+  authMiddleware,
+  updateSocialIcon
 );
 
 export default router;
